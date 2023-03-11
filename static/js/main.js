d52e74a964d3959f3fe04112081366866c767fed
Ram Dayal <84917062+rdtech2020@users.noreply.github.com>
2023-03-12 02:30:46 +0530

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+    "use strict";
+
+
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+
+
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+})(jQuery);
